424f4d9beefeb38bf5a6b6105f2f8b94c8f9f529
John Tolczyk <johntolczyk@gmail.com>
2023-09-06 22:56:58 -0500

    Add files via upload

--- movie tickets.py
+++ movie tickets.py
@@ -1,28 +1,34 @@
 # movie tickets
 
 prompt = "\nWelcome!! Our featured film today is Saw! Please tell us your age: "
-prompt +="\n(Enter'quit to exit.): "
+prompt +="\nLimit 4 tickets per party.(Enter'quit to exit.): "
 
-active = True
+guests = []
 
-while active:
+while True:
     age = input(prompt)
-    
     if age == 'quit':
-              break
-    
+       break
     age = int(age)
-    
     if age < 4:
-        price = 0
+           price = 0    
     elif age < 18:
-            price = 5
+           price = 5
     elif age < 65:
-            price = 15
+             price = 15
     elif age >= 65:
-            price = 7
-
+             price = 7
+    
     print(f"Your admission cost is ${price}.")
+    guests.append(price)
+    if len(guests) >= 4:
+       print("You have reached your limit on tickets.")
+       print(f"Your total is ${sum(guests)}.")
+       break
+       
+
+
+
 
     
 
